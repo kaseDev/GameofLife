dcb498918213140d4ae88fda699559352e90b858
kaseDev <rick.kase@gmail.com>
2018-08-29 22:47:13 -0400

    Initial commit

---
+++ src/main/java/Board.java
@@ -0,0 +1,72 @@
+import javafx.beans.property.BooleanProperty;
+
+public class Board {
+
+	private BooleanProperty field[][];
+	private boolean buffer[][];
+	private int width, height;
+
+	public Board(int width, int height) {
+		this.width = width;
+		this.height = height;
+		field = new BooleanProperty[width][height];
+		buffer = new boolean[width][height];
+	}
+
+	/**
+	 * Calculates the state of the entire board for the next step and then
+	 * stores refreshes the board with the new values.
+	 */
+	private void calculateNextStep() {
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < height; j++)
+				buffer[i][j] = isAlive(i, j);
+		for (int i = 0; i < width; i++)
+			for (int j = 0; j < height; j++)
+				field[i][j].set(buffer[i][j]);
+		buffer = new boolean[width][height];
+	}
+
+	/**
+	 * returns a boolean value representing whether the cell at the
+	 * provided coordinates is alive or dead.
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	private boolean isAlive(int i, int j) {
+		int neighborCount = liveNeighbors(i, j);
+		if (field[i][j].get())
+			return neighborCount >= 2 && neighborCount <= 3;
+		return neighborCount == 3;
+	}
+
+	/**
+	 * returns an integer representing the number of neighbors this
+	 * cell has that are currently alive. Wrapping is built in.
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public int liveNeighbors(int i, int j) {
+		int liveNeighborCount = 0;
+		if (field[(i - 1 + height) % height][(j - 1 + width) % width].get())
+			liveNeighborCount++;
+		if (field[(i - 1 + height) % height][j].get())
+			liveNeighborCount++;
+		if (field[(i - 1 + height) % height][j + 1 % width].get())
+			liveNeighborCount++;
+		if (field[i][(j - 1 + width) % width].get())
+			liveNeighborCount++;
+		if (field[i][j + 1 % width].get())
+			liveNeighborCount++;
+		if (field[i + 1 % height][(j - 1 + width) % width].get())
+			liveNeighborCount++;
+		if (field[i + 1 % height][j].get())
+			liveNeighborCount++;
+		if (field[i + 1 % height][j + 1 % width].get())
+			liveNeighborCount++;
+		return liveNeighborCount;
+	}
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		FXMLLoader loader = new FXMLLoader();
+		try {
+			loader.setLocation(getClass().getResource("main.fxml"));
+			Parent parent = loader.load();
+			Scene scene = new Scene(parent);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
